ee65418caa3a04c339d442a11b4328361f853af8
Henrik Brix Andersen <henrik@brixandersen.dk>
2010-08-28 12:52:52 +0000

    Gracefully handle failed GET requests, happens too often

--- rss2html.pl
+++ rss2html.pl
@@ -12,15 +12,16 @@ use LWP::UserAgent;
 use XML::RSS;
 
 # Script arguments
-my ($url, $db, $output, $locale, $background);
+my ($url, $title, $db, $output, $locale, $background);
 GetOptions('url|u=s'        => \$url,
+		   'title|t=s'      => \$title,
 		   'db|d=s'         => \$db,
 		   'output|o=s'     => \$output,
 		   'locale|l=s'     => \$locale,
 		   'background|b=s' => \$background,
 		   'help|h'         => sub { usage(); exit });
 
-unless ($url && $db && $locale) {
+unless ($url && $title && $db && $locale) {
 	usage();
 	exit(1);
 }
@@ -37,54 +38,58 @@ DateTime->DefaultLocale($locale);
 # Fetch RSS
 my $browser = LWP::UserAgent->new;
 my $response = $browser->get($url);
-die "GET $url failed: ", $response->status_line unless ($response->is_success);
+my $status = $response->is_success ? '' : $response->status_line;
 
-# Parse RSS
-my $rss = XML::RSS->new;
-$rss->parse($response->content);
-
-# Prepare DB
+# DB connection
 my $dbh = DBI->connect("dbi:SQLite:$db") || die "Cannot connect to $db: $DBI::errstr";
-my $ch = $dbh->prepare('SELECT publishDate, updated FROM rss2html WHERE guid=?') or die "Couldn't prepare count statement: " . $dbh->errstr;
-my $ih = $dbh->prepare('INSERT INTO rss2html (guid, link, publishDate, title, text) VALUES (?, ?, ?, ?, ?)') or die "Couldn't prepare insert statement: " . $dbh->errstr;
-my $uh = $dbh->prepare('UPDATE rss2html SET link=?, publishDate=?, title=?, text=?, updated=? WHERE guid=?') or die "Couldn't prepare update statement: " . $dbh->errstr;
 
-# Update DB
-foreach my $item (@{$rss->{'items'}}) {
-	my $guid;
-	my $link;
+if ($response->is_success) {
+	# Parse RSS
+	my $rss = XML::RSS->new;
+	$rss->parse($response->content);
 
-	if ($item->{'permaLink'}) {
-		$guid = $item->{'permaLink'};
-		$link = $item->{'permaLink'};
-	} else {
-		$guid = $item->{'guid'};
-		$link = $item->{'link'};
+	# Prepare DB
+	my $ch = $dbh->prepare('SELECT publishDate, updated FROM rss2html WHERE guid=?') or die "Couldn't prepare count statement: " . $dbh->errstr;
+	my $ih = $dbh->prepare('INSERT INTO rss2html (guid, link, publishDate, title, text) VALUES (?, ?, ?, ?, ?)') or die "Couldn't prepare insert statement: " . $dbh->errstr;
+	my $uh = $dbh->prepare('UPDATE rss2html SET link=?, publishDate=?, title=?, text=?, updated=? WHERE guid=?') or die "Couldn't prepare update statement: " . $dbh->errstr;
+
+	# Update DB
+	foreach my $item (@{$rss->{'items'}}) {
+		my $guid;
+		my $link;
+
+		if ($item->{'permaLink'}) {
+			$guid = $item->{'permaLink'};
+			$link = $item->{'permaLink'};
+		} else {
+			$guid = $item->{'guid'};
+			$link = $item->{'link'};
+		}
+
+		my $date = str2time($item->{'pubDate'});
+		my $title = $item->{'title'};
+		my $text = $item->{'description'};
+
+		$ch->execute($guid);
+		my ($olddate, $oldupdated) = $ch->fetchrow_array();
+
+		if ($olddate) {
+			my $updated = ($olddate eq $date) ? 0 : 1;
+			$uh->execute($link, $date, $title, $text, $oldupdated ? $oldupdated : $updated, $guid) or die "Couldn't update row: " . $dbh->errstr;
+		} else {
+			$ih->execute($guid, $link, $date, $title, $text) or die "Couldn't insert row: " . $dbh->errstr;
+		}
 	}
+	$ch->finish;
+	$ih->finish;
+	$uh->finish;
 
-	my $date = str2time($item->{'pubDate'});
-	my $title = $item->{'title'};
-	my $text = $item->{'description'};
-
-	$ch->execute($guid);
-	my ($olddate, $oldupdated) = $ch->fetchrow_array();
-
-	if ($olddate) {
-		my $updated = ($olddate eq $date) ? 0 : 1;
-		$uh->execute($link, $date, $title, $text, $oldupdated ? $oldupdated : $updated, $guid) or die "Couldn't update row: " . $dbh->errstr;
-	} else {
-		$ih->execute($guid, $link, $date, $title, $text) or die "Couldn't insert row: " . $dbh->errstr;
-	}
+	# Delete rows older than 24 hours
+	my $dh = $dbh->prepare('DELETE FROM rss2html WHERE publishDate < ?') or die "Couldn't prepare delete statement: " . $dbh->errstr;
+	my $limit = DateTime->now(time_zone => $timezone)->subtract(hours => $hours)->epoch;;
+	$dh->execute($limit) or die "Couldn't delete rows: " . $dbh->errstr;
+	$dh->finish;
 }
-$ch->finish;
-$ih->finish;
-$uh->finish;
-
-# Delete rows older than 24 hours
-my $dh = $dbh->prepare('DELETE FROM rss2html WHERE publishDate < ?') or die "Couldn't prepare delete statement: " . $dbh->errstr;
-my $limit = DateTime->now(time_zone => $timezone)->subtract(hours => $hours)->epoch;;
-$dh->execute($limit) or die "Couldn't delete rows: " . $dbh->errstr;
-$dh->finish;
 
 # Select rows
 my $sh = $dbh->prepare('SELECT link, publishDate, updated, title, text FROM rss2html ORDER BY publishDate DESC') or die "Couldn't prepare select statement: " . $dbh->errstr;
@@ -113,8 +118,9 @@ $dbh->disconnect;
 my $template = HTML::Template->new(filename => 'templates/rss2html.tmpl');
 $template->param(BACKGROUND => "background: $background;") if ($background);
 $template->param(ENCODING => $encoding);
-$template->param(TITLE => $rss->{'channel'}->{'title'});
-$template->param(SUBTITLE => $rss->{'channel'}->{'title'});
+$template->param(TITLE => $title);
+$template->param(SUBTITLE => $title);
+$template->param(STATUS => $status);
 $template->param(ITEM_LOOP => \@item_loop);
 
 my $timestamp = encode($encoding, DateTime->now(time_zone => $timezone)->strftime($timeformat));
@@ -136,6 +142,7 @@ Usage: $0 [options]
 
 Options:
 --url, -u          The URL of the RSS feed
+--title, -t        The title string
 --db, -d           The database filename
 --output, -o       The output filename (or - for stdout)
 --locale, -l       The locale (e.g. da, en_US, ...)
